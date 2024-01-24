ab351491058ffd3d83a2abcb7a8626b7f4f27cc9
mukil <mukilathuruvan5241@gmail.com>
2024-01-24 12:57:26 +0530

    useCallback intro

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import { useMemo, useState } from "react";
+import { useCallback, useMemo, useState } from "react";
 import ChildOne, { ChildThree, ChildTwo } from "./components/Child";
 
 const App = () => {
   const [count, setCount] = useState(10);
   const [age, setAge] = useState(10);
   console.log("parent");
-
-  const person = useMemo(() => {
-    return {
-      name: "ragnar",
-      age: 21,
-    };
-  }, [count]);
-
+  const handleChange = useCallback(() => {
+    setAge(age + 1);
+  }, [age]);
   return (
     <main>
       <div>Parent</div>
       <button onClick={() => setCount(count + 1)}>change</button>
       <button onClick={() => setAge(age + 1)}>change</button>
+
       <ChildOne count={count} />
       <ChildTwo />
-      <ChildThree person={person} />
+      <ChildThree handleChange={handleChange} />
     </main>
   );
 };
